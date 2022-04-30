6f69e821ee20ec0b685e8f391cca082a586727f0
gncEray <62249421+gncEray@users.noreply.github.com>
2022-04-30 13:20:49 +0300

    Add files via upload

---
+++ BasicMathematicalOperations.kt
@@ -0,0 +1,17 @@
+package com.chucky.kotlinfundamentals
+
+fun mySum(n1 : Int, n2 : Int) {
+    println("result = ${n1 + n2}")
+}
+
+fun myMultiply(n1: Int, n2: Int) : Int {
+    val result = n1 * n2
+    return result //or directly return n1 * n2
+}
+
+
+fun main() {
+    mySum(3, 5)
+
+    println("Result = ${myMultiply(3,5)}")
+}
